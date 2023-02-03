b08c1a16319ec790427f80deafa43366e4b0faf2
E-commerce <ferney300810@gmail.com>
2023-02-03 17:12:53 -0500

    Proyecto1

---
+++ stock.js
@@ -0,0 +1,14 @@
+let stockProductos = [
+    { id: 1, nombre: "Bateria Recargable", cantidad: 1, desc: "Producto de excelente calida y garantia", precio: 439, img: './PartesElectricas/PE-Bateria.jpg' },
+    { id: 2, nombre: "Bobina de encendiddo", cantidad: 1, desc: "Producto confiable en material de alta calidad", precio: 139, img: './PartesElectricas/PE-Bobina.jpg' },
+    { id: 3, nombre: "Bombillo Led", cantidad: 1, desc: "Producto confiable en material de alta calidad", precio: 76, talle: "M", img: './PartesElectricas/PE-Bombillo.jpg' },
+    { id: 4, nombre: "Direccionales originales", cantidad: 1, desc: "Producto confiable en material de alta calidad", precio: 14, talle: "M", img: './PartesElectricas/PE-Direccionales.jfif' },
+    { id: 5, nombre: "Farola Default", cantidad: 1, desc: "Producto confiable en material de alta calidad", precio: 120, talle: "S", img: './PartesElectricas/PE-Farola.jpg' },
+    { id: 6, nombre: "Stop Default", cantidad: 1, desc: "Producto confiable en material de alta calidad", precio: 150, talle: "S", img: './PartesElectricas/PE-Stop2.jpg' },
+    { id: 7, nombre: "Biela Standar", cantidad: 1, desc: "Producto confiable en material de alta calidad", precio: 58, talle: "L", img: './PartesMecanicas/PM-Biela.jpg' },
+    { id: 8, nombre: "Cilindro", cantidad: 1, desc: "Producto confiable en material de alta calidad", precio: 75, talle: "L", img: './PartesMecanicas/PM-Cilindro.jpg' },
+    { id: 9, nombre: "Discos de Cluth", cantidad: 1, desc: "Producto confiable en material de alta calidad", precio: 50, talle: "M", img: './PartesMecanicas/PM-Discos.jpg' },
+    { id: 10, nombre: "Monoshock Standar", cantidad: 1, desc: "Producto confiable en material de alta calidad", precio: 90, talle: "M", img: './PartesMecanicas/PM-Monoshok.jpg' },
+    { id: 11, nombre: "Piston", cantidad: 1, desc: "Producto confiable en material de alta calidad", precio: 78, talle: "S", img: './PartesMecanicas/PM-Piston.jpg' },
+    { id: 12, nombre: "Volanta", cantidad: 1, desc: "Producto confiable en material de alta calidad", precio: 79, talle: "S", img: './PartesMecanicas/PM-Volanta2.jpg' },
+]
